ce62d2aa3361faa3918bb8f6a72577cd5911d112
root <root@ivozcluster2.local>
2018-12-20 16:36:33 +0000

    Added untracked files

---
+++ console/clean_datadb_from_unused_tp.go
@@ -0,0 +1,63 @@
+/*
+Real-time Online/Offline Charging System (OCS) for Telecom & ISP environments
+Copyright (C) ITsysCOM GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package console
+
+import "github.com/cgrates/cgrates/apier/v1"
+
+func init() {
+	c := &CleanDataDbUnusedTp{
+		name:      "clean_datadb_from_unused_tp",
+		rpcMethod: "ApierV1.CleanDataDbUnusedTp",
+	}
+	commands[c.Name()] = c
+	c.CommandExecuter = &CommandExecuter{c}
+}
+
+// Commander implementation
+type CleanDataDbUnusedTp struct {
+	name      string
+	rpcMethod string
+	rpcParams *v1.AttrCleanDataDbUnusedTp
+	rpcResult string
+	*CommandExecuter
+}
+
+func (self *CleanDataDbUnusedTp) Name() string {
+	return self.name
+}
+
+func (self *CleanDataDbUnusedTp) RpcMethod() string {
+	return self.rpcMethod
+}
+
+func (self *CleanDataDbUnusedTp) RpcParams(reset bool) interface{} {
+	if reset || self.rpcParams == nil {
+		self.rpcParams = &v1.AttrCleanDataDbUnusedTp{}
+	}
+	return self.rpcParams
+}
+
+func (self *CleanDataDbUnusedTp) PostprocessRpcParams() error {
+	return nil
+}
+
+func (self *CleanDataDbUnusedTp) RpcResult() interface{} {
+	var s string
+	return &s
+}
